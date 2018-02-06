92e7ae8ad87df3536ec94014e1b8fdac3925770d
Groverjun <1250901900@qq.com>
2018-02-06 18:34:19 +0800

    add

---
+++ js/index.js
@@ -0,0 +1,54 @@
+/*banner*/
+let vm=new Vue({
+	el:"#html",
+	data:{
+		html:"",
+		name:"",
+		bannerImg:["img/11.jpg","img/22.jpg","img/33.jpg","img/44.jpg","img/55.jpg"]
+	},
+	mounted:function(){
+		var swiper = new Swiper('.swiper-container', {
+                pagination: '.swiper-pagination',
+                paginationClickable: true,
+                loop: false,
+                speed: 600,
+                autoplay: 1500,
+                observer: true,
+				observeParents: true,
+				autoplayDisableOnInteraction : false,
+                onTouchEnd: function() {
+                    swiper.startAutoplay()
+                }
+            });
+	},
+	methods:{
+		getBanner(){
+			var _this= this;
+			this.dataImg(_this,this.$refs.uploadingBannerImg,function(data){
+				console.log(0)
+			})			
+		},
+		/*上传图片**/
+		dataImg(_this,input_file, get_data){
+            if (typeof (FileReader) === 'undefined') {  
+                alert("抱歉，你的浏览器不支持 FileReader，不能将图片转换为Base64，请使用现代浏览器操作！");  
+            } else {  
+                try {  
+                    var file = input_file.files[0];  
+                    if (!/image\/\w+/.test(file.type)) {  
+                        alert("请确保文件为图像类型");  
+                        return false;  
+                    }  
+                    var reader = new FileReader();  
+                    reader.onload = function () {  
+                        get_data(this.result);  
+                    }  
+                    reader.readAsDataURL(file);  
+                } catch (e) {  
+                    alert('图片转Base64出错啦！' + e.toString())  
+                }  
+            }					
+		}
+	}
+	
+})
